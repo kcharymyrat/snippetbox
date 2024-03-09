6a7cd64b3200c3010f29ab638ea267f5416c01be
Charymyrat Garryyev <kcharymyrat@gmail.com>
2024-03-09 11:55:01 +0500

    Create users db table, User and UserModel stucts, prepared templates for handlers and routes

---
+++ internal/models/users.go
@@ -0,0 +1,34 @@
+package models
+
+import (
+	"database/sql"
+	"time"
+)
+
+type User struct {
+	ID             int
+	Name           string
+	Email          string
+	HashedPassword []byte
+	Created        time.Time
+}
+
+type UserModel struct {
+	DB *sql.DB
+}
+
+func (m *UserModel) Insert(name, email, password string) error {
+	return nil
+}
+
+func (m *UserModel) Authenticate(email, password string) (int, error) {
+	return 0, nil
+}
+
+func (m *UserModel) Exists(email string) (bool, error) {
+	return false, nil
+}
+
+func (m *UserModel) Get(id int) (*User, error) {
+	return nil, nil
+}
