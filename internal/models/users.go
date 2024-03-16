7388749103b9bb135f567fae89d2b0bdf43fecd7
Charymyrat Garryyev <kcharymyrat@gmail.com>
2024-03-16 07:55:05 +0500

    Implemented authentication using Context

--- internal/models/users.go
+++ internal/models/users.go
@@ -71,8 +71,11 @@ func (m *UserModel) Authenticate(email, password string) (int, error) {
 
 }
 
-func (m *UserModel) Exists(email string) (bool, error) {
-	return false, nil
+func (m *UserModel) Exists(id int) (bool, error) {
+	var exists bool
+	stmt := "SELECT EXISTS(SELECT true FROM users WHERE id = ?)"
+	err := m.DB.QueryRow(stmt, id).Scan(&exists)
+	return exists, err
 }
 
 func (m *UserModel) Get(id int) (*User, error) {
